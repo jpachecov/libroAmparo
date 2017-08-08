ed6f716dc14e2422778659e160fc825771533220
Jean Pierre <la.parte.imaginaria@gmail.com>
2017-08-08 16:25:31 -0500

    Inicio header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HeaderComponent } from './header.component';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
