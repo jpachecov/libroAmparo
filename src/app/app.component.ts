c774df325cd1d87052e4f7e5cfb9fc615b8a1f40
Jean Pierre <la.parte.imaginaria@gmail.com>
2017-08-21 19:16:48 -0500

    avance menu velocity y jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,9 @@ import {
   state,
   style,
   animate,
-  transition
+  transition,
+  group,
+  query
 } from '@angular/animations';
 
 
@@ -14,7 +16,7 @@ import { Libro } from './libro/libro';
 import { LibroService } from './servicios/libros-service';
 import { Router } from '@angular/router';
 
-
+declare var $ :any;
 
 @Component({
   selector: 'app-root',
@@ -26,18 +28,31 @@ import { Router } from '@angular/router';
     '../font-awesome-4.7.0/css/font-awesome.min.css'
   ],
   animations:[
+  /*
     trigger('menuState', [
       state('activo', style({
-        transform:'translateX(0)'
       })),
       state('inactivo', style({
+
+      })),
+      transition('activo => inactivo', 
+        group([
+          
+          query(".menu-capitulos",[animate('500ms ease-out', style({
         transform:'translateX(-350px)',
         opacity:0,
         display:'none'
-      })),
-      transition('activo => inactivo', animate('300ms ease-out')),
+       }))],  {optional:true}),
+
+          query(".contenido-capitulo",[animate('500ms ease-out', style({paddingLeft:"100px"}))],  {optional:true})
+
+        ]),
+
+
+         animate('300ms ease-out')),
       transition('inactivo => activo', animate('300ms ease-in'))
     ])
+    */
   ]
 })
 export class AppComponent {
@@ -45,18 +60,48 @@ export class AppComponent {
   libro: Libro;
   capituloSel : Capitulo;
 
-  menuState:string = "activo";
   onSelect(capitulo : Capitulo) : void {
       this.capituloSel = capitulo;
       this.router.navigate(['/capitulo', this.capituloSel.id]);
   }
 
-  mueveMenu(): void {
-    if(this.menuState == "activo"){
-      this.menuState = "inactivo";
-    } else {
-      this.menuState = "activo";
-    }
+  showMenu():void {
+    $(".menu-capitulos").velocity({
+       translateX: "0px",
+    }, {
+      enqueue : false,
+      duration: 250,
+      complete: function(){
+        //$(this).css("display", "none");
+        $(this).removeClass("menu-hide");
+      }
+    });
+    // Padding del contenido del captitulo
+    $(".contenido-capitulo").velocity({
+      paddingLeft: "350px",
+    }, {
+      duration: 200
+    });
+
+  }
+
+  hideMenu():void {
+    $(".menu-capitulos").velocity({
+       translateX: "-250px",
+    }, {
+      enqueue : false,
+      duration: 200,
+      complete: function(){
+        //$(this).css("display", "none");
+        $(this).addClass("menu-hide");
+      }
+    });
+    // Padding del contenido del captitulo
+    $(".contenido-capitulo").velocity({
+      paddingLeft: "100px",
+    }, {
+      duration: 250
+    });
   }
   constructor(private libroService:LibroService, private router: Router){
     this.libro = this.libroService.getLibroAmparo();
